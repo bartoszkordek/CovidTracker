66468bc0f93b55d28b56a787fa626df465838c91
gzacharski <g.zacharski@wp.pl>
2020-10-31 14:58:07 +0100

    Add user-account-service

---
+++ user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/UserAccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.project.aggregator.api.useraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountServiceApplication.class, args);
+	}
+
+}

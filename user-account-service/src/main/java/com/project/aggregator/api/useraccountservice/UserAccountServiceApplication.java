6cd682368c6848ca1c103ddcf60a3c461ba79e0c
gzacharski <g.zacharski@wp.pl>
2020-10-31 16:28:01 +0100

    Update of application properties

--- user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/UserAccountServiceApplication.java
+++ user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/UserAccountServiceApplication.java
@@ -3,6 +3,7 @@ package com.project.aggregator.api.useraccountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient

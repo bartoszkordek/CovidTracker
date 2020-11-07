5dc026c847dccb0a2b6cfc74488aeb446dfc071a
gzacharski <g.zacharski@wp.pl>
2020-11-07 19:38:57 +0100

    Covid19api.com Listener startup

---
+++ covid19apicom-listener/src/main/java/com/covid19/api/listener/Covid19apicomListenerApplication.java
@@ -0,0 +1,13 @@
+package com.covid19.api.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19apicomListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19apicomListenerApplication.class, args);
+	}
+
+}
---
+++ covid19apicom-listener/src/test/java/com/covid19/api/listener/Covid19apicomListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid19.api.listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19apicomListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

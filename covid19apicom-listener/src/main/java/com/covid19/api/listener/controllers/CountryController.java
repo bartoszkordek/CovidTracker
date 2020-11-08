0b438cf91695b8029c1e9360e540b200563a0cf1
gzacharski <g.zacharski@wp.pl>
2020-11-08 14:15:01 +0100

    Add controller and simple response model

---
+++ covid19apicom-listener/src/main/java/com/covid19/api/listener/controllers/CountryController.java
@@ -0,0 +1,2 @@
+package com.covid19.api.listener.controllers;public class CountryController {
+}
---
+++ covid19apicom-listener/src/main/java/com/covid19/api/listener/model/CovidCountryResponse.java
@@ -0,0 +1,68 @@
+package com.covid19.api.listener.model;
+
+public class CovidResponse {
+
+    private String country;
+    private String countryCode;
+    private int confirmed;
+    private int deaths;
+    private int recovered;
+    private int active;
+    private String date;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(int confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        this.deaths = deaths;
+    }
+
+    public int getRecovered() {
+        return recovered;
+    }
+
+    public void setRecovered(int recovered) {
+        this.recovered = recovered;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}

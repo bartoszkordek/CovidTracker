c1e140b108bf57139818ec9d795b94db315a6a0b
gzacharski <g.zacharski@wp.pl>
2020-11-16 18:05:20 +0100

    Added: test rest controller pt2

--- search-microservice/src/main/java/com/covid/search/controller/CountryController.java
+++ search-microservice/src/main/java/com/covid/search/controller/CountryController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/search")
-public class SearchController {
+@RequestMapping("/country")
+public class CountryController {
 
-    @GetMapping("/country")
+    @GetMapping("/test")
     public String getResponse(){
         return "it's ok";
     }

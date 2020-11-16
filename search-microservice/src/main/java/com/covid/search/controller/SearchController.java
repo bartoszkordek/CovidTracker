f1e9f4fb049f2b6d5d734b09ecac01bcd1fb835d
gzacharski <g.zacharski@wp.pl>
2020-11-16 17:55:33 +0100

    Added: test rest controller

---
+++ search-microservice/src/main/java/com/covid/search/controller/SearchController.java
@@ -0,0 +1,16 @@
+package com.covid.search.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+
+    @GetMapping("/country")
+    public String getResponse(){
+        return "it's ok";
+    }
+}

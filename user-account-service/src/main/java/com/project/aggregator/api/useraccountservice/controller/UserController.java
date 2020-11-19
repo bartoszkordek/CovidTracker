da5346c1faa8525fa344531969afd71684ab52d6
gzacharski <g.zacharski@wp.pl>
2020-11-19 18:03:47 +0100

    Initial commit

--- user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/controller/UserController.java
+++ user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/controller/UserController.java
@@ -3,6 +3,7 @@ package com.project.aggregator.api.useraccountservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class UserController {
     @GetMapping("/status/check")
     public String status(){
         return "Working "+environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "New user was created";
     }
 }

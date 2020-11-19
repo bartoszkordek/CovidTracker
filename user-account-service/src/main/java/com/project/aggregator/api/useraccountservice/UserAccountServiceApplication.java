4a965d769cc9ec82ee86b1fd682a3b43eb1a7027
gzacharski <g.zacharski@wp.pl>
2020-11-19 19:42:17 +0100

    Enable WebSecurity

--- user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/UserAccountServiceApplication.java
+++ user-account-service/src/main/java/com/project/aggregator/api/useraccountservice/UserAccountServiceApplication.java
@@ -3,14 +3,22 @@ package com.project.aggregator.api.useraccountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebSecurity
 public class UserAccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserAccountServiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }

727461115890a2288b9dd512673424601dcc54b5
bartoszkordek <bartosz.kordek10@gmail.com>
2020-12-18 21:43:35 +0100

    endpoint update -> was /poland, now is /seach

--- search-microservice/src/main/java/com/covid/search/controller/StatusController.java
+++ search-microservice/src/main/java/com/covid/search/controller/StatusController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/poland")
+@RequestMapping("/search")
 public class StatusController {
 
     private final CountryServiceImpl countryService;

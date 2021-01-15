fd7b87bae80be9aa94640abdbc7d31b305302a4f
bartoszkordek <bartosz.kordek10@gmail.com>
2021-01-15 16:45:00 +0100

    business logic update, security tests

--- search-microservice/src/main/java/com/covid/search/data/Covid19TrackingNarrativaServiceClient.java
+++ search-microservice/src/main/java/com/covid/search/data/Covid19TrackingNarrativaServiceClient.java
@@ -1,6 +1,7 @@
 package com.covid.search.data;
 
 import com.covid.search.model.CovidCountryResponse;
+import com.covid.search.model.DailyStatisticsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name="covid19-tracking-narrativa-service")
 public interface Covid19TrackingNarrativaServiceClient {
 
-    //make a call to another Covid19TrackingNarrativa microservice
+    //make a call to Covid19TrackingNarrativa microservice
     @GetMapping("/search/{country}/total")
     int getTotalCountry(@PathVariable("country") final String countryName,
                         @RequestParam(required = false) final String from,
@@ -20,5 +21,12 @@ public interface Covid19TrackingNarrativaServiceClient {
                         @RequestParam(required = false) final String from,
                         @RequestParam(required = false) final String to);
 
+    @GetMapping("/search/{country}/recovered")
+    int getRecoveredCountry(@PathVariable("country") final String countryName,
+                            @RequestParam(required = false) final String from,
+                            @RequestParam(required = false) final String to);
 
+    @GetMapping("/search/{country}")
+    String getDailyStatistics(@PathVariable("country") final String countryName,
+                                               @RequestParam(required = true) final String date);
 }
---
+++ search-microservice/src/main/java/com/covid/search/data/LocalCovidDataServiceClient.java
@@ -0,0 +1,21 @@
+package com.covid.search.data;
+
+import com.covid.search.model.CountryStatisticsResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="local-covid-data-service")
+public interface LocalCovidDataServiceClient {
+
+    //make a call to Local Covid Data microservice
+    @GetMapping("/search/test")
+    String getTest();
+
+    @GetMapping("/search/{country}")
+    String getCountryStatistics(@PathVariable("country") String countryName);
+
+    @GetMapping("/search/today/total/test")
+    int getTodayTotalTest();
+
+}
